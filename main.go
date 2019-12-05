3d5113da607204bd89c8f25acbc824d5df526459
Juan David Adarve <juanda0718@gmail.com>
2019-12-05 16:54:55 -0500

    Added -output parameter.

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
 	"strings"
 )
 
 func main() {
 	var inputFile string
+	var outputFile string
 	var xxxTags string
 	flag.StringVar(&inputFile, "input", "", "path to input file")
+	flag.StringVar(&outputFile, "output", "", "path to output file")
 	flag.StringVar(&xxxTags, "XXX_skip", "", "skip tags to inject on XXX fields")
 
 	flag.Parse()
@@ -23,11 +26,26 @@ func main() {
 		log.Fatal("input file is mandatory")
 	}
 
-	areas, err := parseFile(inputFile, xxxSkipSlice)
+	if len(outputFile) == 0 {
+		log.Fatal("output file is mandatory")
+	}
+
+	// copy input to output
+	data, err := ioutil.ReadFile(inputFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err = writeFile(inputFile, areas); err != nil {
+
+	err = ioutil.WriteFile(outputFile, data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	areas, err := parseFile(outputFile, xxxSkipSlice)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = writeFile(outputFile, areas); err != nil {
 		log.Fatal(err)
 	}
 }
